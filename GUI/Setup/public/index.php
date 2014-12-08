dd6724a7783a9b3ee2acddadf703202fec868730
Christian Johansson <christian@cvj.se>
2014-12-08 10:44:44 +0100

    aom_fw-255 Make select() method on table object

--- GUI/Setup/public/index.php
+++ GUI/Setup/public/index.php
@@ -1,15 +1,5 @@
 <?php
 
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', true);
+require_once(dirname(dirname(dirname(__DIR__))) . '/Application.php');
 
-require_once(dirname(dirname(dirname(__DIR__)))
-    . DIRECTORY_SEPARATOR
-    . 'Application.php');
-
-new \Aomebo\Application(
-    array(
-        \Aomebo\Application::PARAMETER_SITE_PATH =>
-            dirname(__DIR__) . DIRECTORY_SEPARATOR . 'private',
-    )
-);
+new \Aomebo\Application();
