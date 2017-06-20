2ad842c901aeea3152a207b1422c538247252d44
Christian Johansson <christian@mediastrategi.se>
2017-06-20 18:23:00 +0200

    Mostly white-space fixes

--- GUI/Setup/private/Modules/Setup/Module.php
+++ GUI/Setup/private/Modules/Setup/Module.php
@@ -18,64 +18,65 @@ namespace Modules\Setup
         \Aomebo\Runtime\Internationalized
     {
 
-	    /**
-	     * @return string
-	     */
-	    public function execute()
-	    {
-		    \Modules\Html\Module::setTitle(__('Setup', 'setup'));
-		    if (\Aomebo\Dispatcher\System::isHttpPostRequestWithPostData()) {
-			    $config = array(
-				    'database' => array(
-					    'host' => self::_getPostLiterals('database_host'),
-					    'database' => self::_getPostLiterals('database_database'),
-					    'username' => self::_getPostLiterals('database_username'),
-					    'password' => self::_getPostLiterals('database_password'),
-					    'type' => self::_getPostLiterals('database_type'),
-					    'options' => array(
-						    'dsn' => self::_getPostLiterals('database_dsn'),
-					    ),
-				    )
-			    );
+        /**
+         * @return string
+         */
+        public function execute()
+        {
+            \Modules\Html\Module::setTitle(__('Setup', 'setup'));
+            if (\Aomebo\Dispatcher\System::isHttpPostRequestWithPostData()) {
+                $config = array(
+                    'database' => array(
+                        'host' => self::_getPostLiterals('database_host'),
+                        'database' => self::_getPostLiterals('database_database'),
+                        'username' => self::_getPostLiterals('database_username'),
+                        'password' => self::_getPostLiterals('database_password'),
+                        'type' => self::_getPostLiterals('database_type'),
+                        'options' => array(
+                            'dsn' => self::_getPostLiterals('database_dsn'),
+                        ),
+                    )
+                );
 
-			    $data = \Aomebo\Configuration::generatePhpData($config);
-			    \Aomebo\Dispatcher\System::setHttpHeaderField(
-				    'Content-Disposition',
-				    'attachment; filename="configuration.php"'
-			    );
-			    \Aomebo\Dispatcher\System::setHttpHeaderField(
-				    'Content-Type',
-				    'text/php'
-			    );
-			    \Aomebo\Dispatcher\System::outputHttpHeaders();
-			    die($data);
+                $data = \Aomebo\Configuration::generatePhpData($config);
+                \Aomebo\Dispatcher\System::setHttpHeaderField(
+                    'Content-Disposition',
+                    'attachment; filename="configuration.php"'
+                );
+                \Aomebo\Dispatcher\System::setHttpHeaderField(
+                    'Content-Type',
+                    'text/php'
+                );
+                \Aomebo\Dispatcher\System::outputHttpHeaders();
+                echo $data;
+                exit;
 
-		    } else {
-			    $submit = array(
-				    'database_host' => '',
-				    'database_database' => '',
-				    'database_username' => '',
-				    'database_password' => '',
-				    'database_type' => '',
-				    'database_dsn' => '',
-				    'locale' => '',
-				    'database_autoinstall' => '',
-				    'database_autouninstall' => '',
-				    'database_autoupdate' => '',
-			    );
-		    }
+            } else {
+                $submit = array(
+                    'database_host' => '',
+                    'database_database' => '',
+                    'database_username' => '',
+                    'database_password' => '',
+                    'database_type' => '',
+                    'database_dsn' => '',
+                    'locale' => '',
+                    'database_autoinstall' => '',
+                    'database_autouninstall' => '',
+                    'database_autoupdate' => '',
+                );
+            }
 
-		    $view = self::_getTwigView();
-		    $view->attachVariables(array(
-			    'submit' => $submit,
-			    'locales' => \Aomebo\Internationalization\System::getLocalesFromDirectory(
-				    __DIR__ . '/Locales'),
+            $view = self::_getTwigView();
+            $view->attachVariables(array(
+                'submit' => $submit,
+                'locales' => \Aomebo\Internationalization\System::getLocalesFromDirectory(
+                    __DIR__ . '/Locales'),
                 'structure' => \Aomebo\Configuration::getStructure(),
 
-		    ));
-		    $view->setFile('views/view.twig');
-		    return $view->parse();
-	    }
+            ));
+            $view->setFile('views/view.twig');
+            return $view->parse();
+        }
 
         /**
          * Should return an associative array containing page => page data or boolean false.
@@ -96,23 +97,23 @@ namespace Modules\Setup
                             array(
                                 'key' => 'body',
                                 'value' => array(
-	                                array(
-		                                'key' => 'wrapper',
-		                                'value' => array(
-			                                array(
-				                                'key' => 'header',
-				                                'value' => '',
-			                                ),
-			                                array(
-				                                'key' => 'setup',
-				                                'value' => '',
-			                                ),
-			                                array(
-				                                'key' => 'footer',
-				                                'value' => '',
-			                                )
-		                                ),
-	                                ),
+                                    array(
+                                        'key' => 'wrapper',
+                                        'value' => array(
+                                            array(
+                                                'key' => 'header',
+                                                'value' => '',
+                                            ),
+                                            array(
+                                                'key' => 'setup',
+                                                'value' => '',
+                                            ),
+                                            array(
+                                                'key' => 'footer',
+                                                'value' => '',
+                                            )
+                                        ),
+                                    ),
                                 ),
                             ),
                         ),
--- GUI/Setup/private/Modules/Test/Associatives/script.js
+++ GUI/Setup/private/Modules/Test/Associatives/script.js
@@ -6,36 +6,36 @@
 
 function testAjax()
 {
-	$('#test #ajax-tests .success').hide();
-	$('#test #ajax-tests .error').hide();
-	$.ajax(_AJAX_URI_, {
-		cache: false,
-		method: 'POST',
-		data: {
-			page: 'test'
-		},
-		success: function(given) {
-			var expected = $('#test #ajax-tests .expected').html();
-			$('#test #ajax-tests .given').html(given);
-			if (given == expected) {
-				$('#test #ajax-tests .success').show();
-			} else {
-				console.log('given doesnt equal expected');
-				console.log(given);
-				console.log(expected);
-				$('#test #ajax-tests .error').show();
-			}
-		},
-		error: function(given) {
-			$('#test #ajax-tests .given').html(given);
-			$('#test #ajax-tests .error').show();
-		}
-	});
+    $('#test #ajax-tests .success').hide();
+    $('#test #ajax-tests .error').hide();
+    $.ajax(_AJAX_URI_, {
+        cache: false,
+        method: 'POST',
+        data: {
+            page: 'test'
+        },
+        success: function(given) {
+            var expected = $('#test #ajax-tests .expected').html();
+            $('#test #ajax-tests .given').html(given);
+            if (given == expected) {
+                $('#test #ajax-tests .success').show();
+            } else {
+                console.log('given doesnt equal expected');
+                console.log(given);
+                console.log(expected);
+                $('#test #ajax-tests .error').show();
+            }
+        },
+        error: function(given) {
+            $('#test #ajax-tests .given').html(given);
+            $('#test #ajax-tests .error').show();
+        }
+    });
 }
 
 /**
  *
  */
 $(document).ready(function(event) {
-	testAjax();
+    testAjax();
 });
